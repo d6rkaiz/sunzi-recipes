08a08c4743253f0f48784a5fd499dac61dce7425
d6rkaiz <d6rkaiz@gmail.com>
2013-04-27 19:35:09 +0900

    mistake if

--- recipes/rbenv-ruby.sh
+++ recipes/rbenv-ruby.sh
@@ -5,7 +5,7 @@ if [ ! -d /usr/local/rbenv ]; then
     echo 'rbenv is not installed, skiping...'
 else
     echo "Compiling Ruby. take a while..."
-    if which rbenv > /dev/null; then
+    if ! which rbenv > /dev/null; then
         source /etc/profile.d/rbenv.sh
     fi
     rbenv install $1

cedb50e87c0e529f0ae1da69ebab741442ad9abd
d6rkaiz <d6rkaiz@gmail.com>
2013-05-11 03:40:28 +0900

    mysql character set change to utf8mb4 from utf8

--- recipes/mysql.sh
+++ recipes/mysql.sh
@@ -58,13 +58,15 @@ EOM
     rm -f $targetfile
     cat >> $targetfile <<EOM
 [client]
-default-character-set=utf8
+default-character-set = utf8mb4
 
 [mysql]
-default-character-set=utf8
+default-character-set = utf8mb4
 
 [mysqld]
-character-set-server=utf8
+character-set-client-handshake = FALSE
+character-set-server = utf8mb4
+collation-server = utf8mb4_unicode_ci
 EOM
     sunzi.mute "service mysql start"
     query "show variables like 'innodb_version'"

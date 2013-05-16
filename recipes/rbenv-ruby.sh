14a52b083fbb5edc5fd5a1ceec2416033c005d80
d6rkaiz <d6rkaiz@gmail.com>
2013-05-16 21:29:36 +0900

    change to setup rubygems

--- recipes/rbenv-ruby.sh
+++ recipes/rbenv-ruby.sh
@@ -12,5 +12,12 @@ else
         rbenv install $1
         rbenv global $1
         rbenv rehash
+        sunzi.mute "gem update --system"
+        sunzi.mute "gem install bundler"
+    fi
+    if [ -f ~/.gemrc ]; then
+        echo 'gems already setup, skiping...'
+    else
+        echo 'gem: --no-ri --no-rdoc' > ~/.gemrc
     fi
 fi
--- recipes/ruby-gems.sh
+++ recipes/ruby-gems.sh
@@ -1,17 +1,2 @@
 # gems setup
 # 
-
-if [ ! -d /usr/local/rbenv ]; then
-    echo 'rbenv is not installed, skiping...'
-else
-    if ! which rbenv > /dev/null; then
-        source /etc/profile.d/rbenv.sh
-    fi
-    if [ -f ~/.gemrc ]; then
-        echo 'gems already setup, skiping...'
-    else
-        echo 'gem: --no-ri --no-rdoc' > ~/.gemrc
-        sunzi.mute "gem update --system"
-        sunzi.mute "gem install bundler"
-    fi
-fi

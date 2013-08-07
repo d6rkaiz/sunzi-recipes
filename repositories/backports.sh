0da3b992971c93c2d0e829c454205c6955790171
d6rkaiz <d6rkaiz@gmail.com>
2013-08-07 14:13:40 +0900

    backports repo setup cleanup

--- repositories/backports.sh
+++ repositories/backports.sh
@@ -4,6 +4,5 @@ if [ -f /etc/apt/sources.list.d/backports.list ]; then
   echo 'backports entry already exists'
 else
   echo 'Adding Backports to the apt source list'
-#  echo 'deb http://ftp.jp.debian.org/debian-backports wheezy-backports main' > /etc/apt/sources.list.d/backports.list
   echo 'deb http://ftp.debian.org/debian/ wheezy-backports main' > /etc/apt/sources.list.d/backports.list
 fi

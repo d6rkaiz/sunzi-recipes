b27115246a9ee6425bb86294c23756d77aa37428
d6rkaiz <d6rkaiz@gmail.com>
2014-02-27 18:37:26 +0900

    tweak locale setting

--- recipes/locale.sh
+++ recipes/locale.sh
@@ -1,6 +1,4 @@
-export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-locale-gen --purge en_US.UTF-8
-sed -i 's/^#LANG=/LANG=/' /etc/default/locale
-dpkg-reconfigure -f noninteractive locales
+export LC_ALL=$LANG
+locale-gen --purge $LANG
+dpkg-reconfigure -f noninteractive locales && /usr/sbin/update-locale LANG=$LANG

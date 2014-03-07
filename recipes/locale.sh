eaa92364feb31ffce88bb83dd7599e502715d8a1
d6rkaiz <d6rkaiz@gmail.com>
2014-03-08 02:56:20 +0900

    add LC_ALL

--- recipes/locale.sh
+++ recipes/locale.sh
@@ -1,4 +1,4 @@
 export LANG=en_US.UTF-8
 export LC_ALL=$LANG
 locale-gen --purge $LANG
-dpkg-reconfigure -f noninteractive locales && /usr/sbin/update-locale LANG=$LANG
+dpkg-reconfigure -f noninteractive locales && /usr/sbin/update-locale LANG=$LANG LC_ALL=$LANG

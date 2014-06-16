bf2664afdbdd423e7b0ecb7f6ef3cdc21cc4cafc
d6rkaiz <d6rkaiz@gmail.com>
2014-06-16 15:27:11 +0900

    fix force-yes

--- recipes/php.sh
+++ recipes/php.sh
@@ -12,7 +12,7 @@ if ! sunzi.installed "nginx"; then
 fi
 
 if ! sunzi.installed "php5-common"; then
-    sunzi.mute "apt-get install php5 php5-common php5-apc php5-gd php5-fpm php5-cli"
+    sunzi.mute "apt-get install -qqy php5 php5-common php5-apc php5-gd php5-fpm php5-cli"
     if sunzi.installed "mysql-server-5.6"; then
         if ! sunzi.installed "php5-mysql"; then
             sunzi.install "php5-mysql"
